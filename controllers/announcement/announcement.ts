9d976706cc09f62fcabb6647b6e0a38c14a185b8
AnishSinghChauhan <anishthe01@gmail.com>
2024-11-08 23:32:29 +0530

    add cloudinary

--- controllers/announcement/announcement.ts
+++ controllers/announcement/announcement.ts
@@ -1,21 +1,41 @@
 import e, { Response, NextFunction } from "express";
 import { CustomError, CustomRequest } from "../../types";
 import {Announcement} from '../../models';
-import exp from "constants";
+import fs from 'fs';
+import {cloudinary} from '../../config'
 
-// Create a new announcement
 const createAnnouncement = async (req: CustomRequest, res: Response , next:NextFunction) => {
   const { title, description, poll, createdBy } = req.body;
-  const media = req.files ? (req.files as Express.Multer.File[]).map(file => file.path) : [];
+  const files = req.files as Express.Multer.File[] | undefined;
+  const mediaUrls = []; // cloudinary urls
 
   try {
+    
+    if (files && files.length > 0) {
+      for (const file of files) {
+        const result = await cloudinary.uploader.upload(file.path, {
+          resource_type: 'auto',
+          folder: 'announcements', // cloudinary folder
+        });
+
+        mediaUrls.push(result.secure_url);
+
+        fs.unlink(file.path, (err) => {
+          if (err) {
+            console.error(`Failed to delete local file: ${file.path}`, err);
+          }
+        });
+      }
+    }
+
+
     const newAnnouncement = new Announcement({
       title,
       description,
-      media,
+      media: mediaUrls,
       poll,
       createdBy
-    });
+  });
 
     await newAnnouncement.save();
     res.status(201).json({ message: 'Announcement created successfully', announcement: newAnnouncement });
@@ -24,4 +44,4 @@ const createAnnouncement = async (req: CustomRequest, res: Response , next:NextF
   }
 };
 
-export default createAnnouncement ;
+export default createAnnouncement;
