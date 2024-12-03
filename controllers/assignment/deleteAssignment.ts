39302d745cc7fcfdd757762e83b20fed7a7d7f20
AnishSinghChauhan <anishthe01@gmail.com>
2024-12-03 18:08:56 +0530

    migrate upload services from cloudinary to s3

--- controllers/assignment/deleteAssignment.ts
+++ controllers/assignment/deleteAssignment.ts
@@ -1,7 +1,9 @@
 import { Response, NextFunction } from "express";
 import { CustomError, CustomRequest } from "../../types";
 import { Assignment, User,Classroom } from "../../models";
-import { cloudinary } from "../../config";
+import { S3 } from "../../config"; 
+import { DeleteObjectCommand } from "@aws-sdk/client-s3";
+// import { cloudinary } from "../../config";
 
 const deleteAssignment = async (req: CustomRequest, res: Response, next: NextFunction) => {
   const { id , code } = req.query;
@@ -26,13 +28,32 @@ const deleteAssignment = async (req: CustomRequest, res: Response, next: NextFun
       return next(new CustomError('You are not authorized to delete assignment in this classroom',403));
     }
 
+    // if (assignment.media && assignment.media.length > 0) {
+    //   for (const url of assignment.media) {
+    //     const publicId = url.split('/').pop()?.split('.')[0];
+    //     if (publicId) {
+    //       await cloudinary.uploader.destroy(`assignments/${publicId}`);
+    //     }
+    //   }
+    // }
+
+    const bucketName = process.env.AWS_S3_BUCKET_NAME;
+    if (!bucketName) {
+      throw new CustomError("AWS S3 bucket name is not configured in environment variables.", 500);
+    }
+
     if (assignment.media && assignment.media.length > 0) {
-      for (const url of assignment.media) {
-        const publicId = url.split('/').pop()?.split('.')[0];
-        if (publicId) {
-          await cloudinary.uploader.destroy(`assignments/${publicId}`);
+      const deletePromises = assignment.media.map(async (url) => {
+        const key = url.split(`${bucketName}/`)[1];
+        if (key) {
+          const deleteCommand = new DeleteObjectCommand({
+            Bucket: bucketName,
+            Key: key,
+          });
+          await S3.send(deleteCommand);
         }
-      }
+      });
+      await Promise.all(deletePromises);
     }
 
     await assignment.deleteOne();
